837c8a6b42f95b71ad591a97e1e016dfaf208659
r0ck3r008 <naman.arora@tutanota.com>
2021-04-03 21:41:35 -0400

    Added vector implementation

--- include/vector/vector.h
+++ include/vector/vector.h
@@ -3,9 +3,14 @@
 
 typedef struct Vector {
 	void **arr;
+	int cap, size;
 } Vector;
 
 Vector *vector_init(int);
 void vector_deinit(Vector *);
+void vector_pushBack(Vector *, void *);
+int vector_size(Vector *);
+int vector_cap(Vector *);
+void *vector_at(Vector *, int);
 
 #endif /* VECTOR_H */
--- vector/vector.c
+++ vector/vector.c
@@ -1,4 +1,71 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#include<commons/utils.h>
 #include<vector/vector.h>
+
+/* Private */
+Vector *
+vector_grow(Vector *vec)
+{
+	Vector *new_vec = vector_init(vec->size * 2);
+	for(int i = 0; i < vec->size; i++)
+		new_vec->arr[i] = vec->arr[i];
+
+	vector_deinit(vec);
+	return new_vec;
+}
+
+/* Public */
+Vector *
+vector_init(int size)
+{
+	Vector *vec = (Vector *)calloc(sizeof(Vector), 1);
+	nullExit(vec);
+
+	vec->arr = calloc(sizeof(void *), size);
+	nullExit(vec->arr);
+
+	vec->cap = size;
+
+	return vec;
+}
+
+void
+vector_deinit(Vector *vec)
+{
+	free(vec->arr);
+	free(vec);
+}
+
+void
+vector_pushBack(Vector *vec, void *memb)
+{
+	int size = vec->size;
+	if(size == vec->cap)
+		vec = vector_grow(vec);
+
+	vec->arr[size] = memb;
+	(vec->size)++;
+}
+
+int
+vector_size(Vector *vec)
+{
+	return (vec->size);
+}
+
+int
+vector_cap(Vector *vec)
+{
+	return (vec->cap);
+}
+
+void *
+vector_at(Vector *vec, int pos)
+{
+	if(pos >= vec->size || pos < 0)
+		return NULL;
+
+	return (vec->arr[pos]);
+}
